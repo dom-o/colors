5165c55a4aa07bdc7661593d081d0bd829e477e6
dom-o <m.domonic+github@gmail.com>
2016-03-04 15:00:16 -0500

    Added untested rgb to xyz conversion.

--- colors.py
+++ colors.py
@@ -2,7 +2,38 @@ def rgbtolab(rgb):
   return xyztolab(rgbtoxyz(rgb))
 
 def rgbtoxyz(rgb):
-  pass
+    r = rgb[0]
+    g = rgb[1]
+    b = rgb[2]
+
+    r = r/255
+    g = g/255
+    b = b/255
+
+    if(r > 0.04045):
+        r = pow((r*0.055)/1.055, 2.4)
+    else:
+        r = r/12.92
+
+    if(g > 0.04045):
+        g = pow((g*0.055)/1.055, 2.4)
+    else:
+        g = g/12.92
+
+    if(b > 0.04045):
+        b = pow((b*0.055)/1.055, 2.4)
+    else:
+        b = b/12.92
+
+    r = r * 100
+    g = g * 100
+    b = b * 100
+
+    xyz[0] = r * 0.4124 + g * 0.3576 + b * 0.1805
+    xyz[1] = r * 0.2126 + g * 0.7152 + b * 0.0722
+    xzy[2] = r * 0.0193 + g * 0.1192 + b * 0.9505
+
+    return xyz
 
 def xyztolab(xyz):
     xn= 95.047
