4f9e2c2115a32df6f8940c5b60403ce823baec7f
dom-o <m.domonic+github@gmail.com>
2016-06-30 21:16:48 -0400

    Redesigned web page; fixed server bugs.

--- colors/colors_web.py
+++ colors/colors_web.py
@@ -12,9 +12,19 @@ def show_colors():
 @app.route('/palettes', methods=['POST'])
 def show_palettes():
 #   get colors here
-    print(request.form)
-#   sanitize list (no duplicates, at least two colors)
-#   palette_pairs = get_color_combos(color_list, 6)
+    raw_list = request.form
+    raw_list = raw_list.getlist('color[]')
+    raw_list = list(set(raw_list))
+#   convert list (no duplicates, at least two colors)
+    color_list = [
+        [   int(c.strip('rgb() ').split(',', 2)[0]),
+            int(c.strip('rgb() ').split(',', 2)[1]),
+            int(c.strip('rgb() ').split(',', 2)[2])
+        ]
+        for c in raw_list
+    ]
+    if(len(color_list) >= 2):
+        #palette_pairs = get_color_combos(color_list, 6)
     return render_template('layout.html')#'palettes.html', palette_pairs=palette_pairs)
 
 if __name__ == "__main__":
