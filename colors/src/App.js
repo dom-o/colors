bc061825f5f630b59bba49bdebd5954145599609
Domonic Milesi <dom-o@users.noreply.github.com>
2020-05-17 18:46:05 -0400

    Allow custom main/tail weights; change styling of selected cards.

--- colors/src/App.js
+++ colors/src/App.js
@@ -9,12 +9,15 @@ class App extends Component {
         super();
         this.handleClick = this.handleClick.bind(this);
         this.handleClearClick = this.handleClearClick.bind(this);
+        this.calculate = this.calculate.bind(this)
+        this.contrastOnChange = this.contrastOnChange.bind(this)
         this.state = {
             on: Array(0),
-            combos: Array(0)
+            combos: Array(0),
+            contrast: 50,
         };
     }
-    
+
   render() {
     return (
       <div className="App">
@@ -25,27 +28,49 @@ class App extends Component {
           </p>
         </div>
         <div className="Container">
-            <CardGroup 
+            <CardGroup
                 on={this.state.on}
-                clickFunction={this.handleClick} 
+                clickFunction={this.handleClick}
                 cards={getColorList()}
                 cardHeight={55}
                 cardWidth={55}
             />
             <button onClick={this.handleClearClick}>clear</button>
+            <button onClick={this.calculate}>get color pairs</button>
+
+            <input type="range" id="contrast" min="1" max="99" step="1" value={this.state.contrast} onChange={this.contrastOnChange} />
+
             <CardGroup on={[]} cards={arrToCards(this.state.combos)} cardWidth={70} cardHeight={80} />
         </div>
       </div>
     );
   }
-  
+
     handleClearClick() {
         this.setState({on: [], combos:[]});
     }
-    
+
+    contrastOnChange(e) {
+      this.setState({contrast: e.target.value})
+    }
+
+    calculate() {
+      const colorIndexes = this.state.on;
+
+      const colors = colorIndexes.map(function(a) { return colorList[a] })
+      const rgb = colors.map(hex_to_rgb)
+
+      const contrast = this.state.contrast
+      const main_weight = contrast
+      const tail_weight = 100 - contrast
+      console.log(main_weight)
+      console.log(tail_weight)
+
+      const combos = getColorCombos(rgb, 6, main_weight, tail_weight)
+      this.setState({combos:combos})
+    }
+
     handleClick(key) {
-        var colorIndexes, combos, rgb, colors;
-        
         var newOn = this.state.on;
         if(this.state.on.includes(key)) {
             newOn.splice(newOn.indexOf(key), 1);
@@ -55,25 +80,25 @@ class App extends Component {
         }
 
         this.setState({on:newOn});
-        
-        if(newOn.length >= 2) {
-            colorIndexes = newOn;
 
-            colors = colorIndexes.map(function(a) {return colorList[a];})
-            rgb = colors.map(hex_to_rgb);
-
-            combos = getColorCombos(rgb, 6);
-            this.setState({combos:combos});
-        }
-        else {
-            this.setState({combos:[]});
-        }
+        // if(newOn.length >= 2) {
+        //     colorIndexes = newOn;
+        //
+        //     colors = colorIndexes.map(function(a) {return colorList[a];})
+        //     rgb = colors.map(hex_to_rgb);
+        //
+        //     combos = getColorCombos(rgb, 6, main_weight, tail_weight);
+        //     this.setState({combos:combos});
+        // }
+        // else {
+        //     this.setState({combos:[]});
+        // }
     }
 }
 
 function getColorList() {
     var cards=[], i;
-    
+
     for(i=0; i<colorList.length; i++) {
         cards.push({squares:[colorList[i]]});
     }
@@ -83,10 +108,13 @@ function getColorList() {
 function arrToCards(arr) {
     //arr = [[[r,g,b],[r,g,b]], [[r,g,b],[r,g,b]], [[r,g,b],[r,g,b]]]
     var cards=[], colors=[];
-    cards = arr.map(function(card) {
-        colors = card.map(function(color) { return 'rgb('+color.join(',')+')'; });
-        return {squares: colors};
-    });
+    if (arr) {
+      cards = arr.map(function(card) {
+          colors = card.map(function(color) { return 'rgb('+color.join(',')+')'; });
+          return {squares: colors};
+      });
+    }
+
     return cards;
     //cards = [{squares:['rgb', 'rgb']}, {squares:['rgb','rgb']}]
 }
@@ -94,13 +122,13 @@ function arrToCards(arr) {
 const CardGroup = (props) => {
     var cardGroupStyle = {
         display: 'grid',
-        gridTemplateColumns: 'repeat(9, min-content)'        
+        gridTemplateColumns: 'repeat(9, min-content)'
     };
-    
+
     return (
         <div style={cardGroupStyle}>
-            {props.cards.map((card, index) => 
-                <Card 
+            {props.cards.map((card, index) =>
+                <Card
                     key={index}
                     colorIndex={index}
                     on={props.on.includes(index)}
@@ -111,7 +139,7 @@ const CardGroup = (props) => {
                 />
             )}
         </div>
-    );    
+    );
 }
 
 class Card extends React.Component {
@@ -119,11 +147,11 @@ class Card extends React.Component {
         super(props);
         this.handleClick = this.handleClick.bind(this);
     }
-    
+
     handleClick() {
         if(this.props.clickFunction) this.props.clickFunction(this.props.colorIndex);
     }
-    
+
     render() {
         var cardStyle = {
             display: 'inline-block',
@@ -132,16 +160,18 @@ class Card extends React.Component {
             margin: 5,
             boxSizing: 'border-box',
             overflow: 'hidden',
-            border: this.props.on ? 'thin solid black' : 'thin solid black',
-            borderRadius: this.props.on ? '50% / 50%' : '5% / 5%'
+            border: this.props.on ? 'medium solid grey' : 'thin solid black',
+            // outline: this.props.on ? 'thin solid white' : 'none',
+            boxShadow: this.props.on ? '0 0 0 0.2rem black' : 'none',
+            borderRadius: this.props.on ? '10% / 10%' : '5% / 5%'
         };
         return(
             <div style={cardStyle} onClick={this.handleClick}>
                 {this.props.squares.map(
                     (color, index) => <Square
                         key={index}
-                        color={color} 
-                        height={cardStyle.height/this.props.squares.length} 
+                        color={color}
+                        height={cardStyle.height/this.props.squares.length}
                 />)}
             </div>
         );
@@ -153,11 +183,9 @@ const Square = (props) => {
         backgroundColor: props.color,
         height: props.height
     };
-    
+
     return (
-        <div style={squareStyle}>
-        
-        </div>
+        <div style={squareStyle}></div>
     );
 }
 
--- colors/src/colors.js
+++ colors/src/colors.js
@@ -1,49 +1,49 @@
-const MAIN_WEIGHT = 50;
-const TAIL_WEIGHT = 50;
+// const MAIN_WEIGHT = 50;
+// const TAIL_WEIGHT = 50;
 
 function arrCompare(a, b) {
     var i;
-        
+
         for(i=0; i<a.length && i<b.length;) {
             if (a[i] === b[i]) i++;
             else return a[i]-b[i];
         }
-        
+
         return 0;
 }
 
-export default function getColorCombos(color_list, num_requested) {
+export default function getColorCombos(color_list, num_requested, main_weight, tail_weight) {
     if(color_list.length < 2) {
         return null;
     }
     if(color_list.length === 2) {
         return [color_list];
     }
-    
-    var to_return = [], max_deltaE = -1, furthest=-1, 
+
+    var to_return = [], max_deltaE = -1, furthest=-1,
         pair_list, j, k, pair, compare, color1, color2, new_deltaE=0;
-    
+
     //remove duplicate colors
     color_list = color_list.filter(function(color, index) {
         return color_list.indexOf(color) === index;
     });
-    
+
     //sort the arrays for consistency in results
     color_list = color_list.sort(arrCompare);
     //combinations vs. permutations: still not sure which way to go here. combinations ensure that a head/tail combo and its opposite won't both be recommended
     pair_list = combinations(color_list, 2);
-    
+
     if(num_requested >= pair_list.length) {
         return pair_list;
     }
-    
+
     //find the 2 pairs that are furthest away from each other
     for(j=0; j<pair_list.length; j++) {
         color1 = pair_list[j];
         for(k=j+1; k<pair_list.length; k++) {
             color2 = pair_list[k];
-            new_deltaE = pair_deltaE_2000(color1[0], color1[1], color2[0], color2[1], MAIN_WEIGHT, TAIL_WEIGHT);
-            
+            new_deltaE = pair_deltaE_2000(color1[0], color1[1], color2[0], color2[1], main_weight, tail_weight);
+
             if(max_deltaE < new_deltaE) {
                 max_deltaE = new_deltaE;
                 pair = [color1, color2];
@@ -51,21 +51,21 @@ export default function getColorCombos(color_list, num_requested) {
             new_deltaE = 0;
         }
     }
-    
+
     to_return = pair;
     pair_list.splice(pair_list.indexOf(pair[0]), 1);
     pair_list.splice(pair_list.indexOf(pair[1]), 1);
-    
+
     new_deltaE=0;
     max_deltaE=-1;
-    
+
     while(to_return.length < num_requested) {
         for(j=0; j<pair_list.length; j++) {
             compare = pair_list[j];
             for(k=0; k<to_return.length; k++) {
-                new_deltaE += pair_deltaE_2000(to_return[k][0], to_return[k][1], compare[0], compare[1], MAIN_WEIGHT, TAIL_WEIGHT);
+                new_deltaE += pair_deltaE_2000(to_return[k][0], to_return[k][1], compare[0], compare[1], main_weight, tail_weight);
             }
-            
+
             if(new_deltaE > max_deltaE) {
                 max_deltaE = new_deltaE;
                 furthest = compare;
@@ -76,25 +76,25 @@ export default function getColorCombos(color_list, num_requested) {
         to_return.push(furthest);
         max_deltaE = 0;
     }
-    
-    
+
+
     return to_return;
 }
 
 //taken from https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
 export function hex_to_rgb(hex) {
     var r,g,b,c;
-    
+
     if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
         c= hex.substring(1).split('');
         if(c.length === 3) c= [c[0], c[0], c[1], c[1], c[2], c[2]];
-        
+
         c = '0x'+c.join('');
-        
+
         r = (c>>16)&255;
         g = (c>>8)&255;
         b = c&255;
-        
+
         return[r,g,b];
     }
     throw new Error('bad hex: ' + hex);
@@ -103,15 +103,15 @@ export function hex_to_rgb(hex) {
 //taken from https://gist.github.com/axelpale/3118596
 export function combinations(list, chunk_size) {
     var combs, start, sub_combs, i, j;
-    
+
     if(chunk_size > list.length || chunk_size <= 0) {
         return [];
     }
-    
+
     if(chunk_size === list.length) {
         return list;
     }
-    
+
     if(chunk_size === 1) {
         combs = [];
         for(i=0; i<list.length; i++) {
@@ -119,7 +119,7 @@ export function combinations(list, chunk_size) {
         }
         return combs;
     }
-    
+
     combs=[];
     for(i=0; i<list.length-chunk_size+1; i++) {
         start = list.slice(i, i+1);
@@ -133,15 +133,15 @@ export function combinations(list, chunk_size) {
 
 function pair_deltaE_2000(main1, tail1, main2, tail2, main_weight, tail_weight) {
     var colors, main_deltaE, tail_deltaE, avg_deltaE;
-    
+
     colors = [main1, main2, tail1, tail2].map( color => srgb_to_lab(color));
     main_weight /= 100;
     tail_weight /= 100;
-    
+
     main_deltaE = deltaE_2000(colors[0], colors[1]);
     tail_deltaE = deltaE_2000(colors[2], colors[3]);
     avg_deltaE = ((main_deltaE*main_weight) + (tail_deltaE*tail_weight)) / (main_weight + tail_weight);
-    
+
     return avg_deltaE;
 }
 
@@ -149,7 +149,7 @@ function pair_deltaE_2000(main1, tail1, main2, tail2, main_weight, tail_weight) 
 //referenced https://github.com/signalwerk/colorlab/blob/master/src/CIEDE2000.js
 export function deltaE_2000(color1, color2) {
     var sqrt, sin, pow, atan2, cos, abs, exp, l1, a1, b1, l2, a2, b2, lBarPrime, c1, c2, cBar, g, a1Prime, a2Prime, c1Prime, c2Prime, cBarPrime, h1Prime, h1PrimeTerm, h2Prime, h2PrimeTerm, hBarPrime, deltaHPrime, deltaHPrimeTerm, deltaLPrime, deltaCPrime, deltaBigHPrime, sL, sC, sH, t, deltaTheta, rC, rT, kL, kC, kH;
-    
+
     sqrt = Math.sqrt;
     sin = Math.sin;
     pow = Math.pow;
@@ -163,63 +163,63 @@ export function deltaE_2000(color1, color2) {
     l2= color2[0];
     a2= color2[1];
     b2= color2[2];
-    
-    
+
+
     lBarPrime = (l1+l2) /2;
-    
+
     //c is the chroma for each color
     c1 = sqrt(pow(a1, 2) + pow(b1, 2));
     c2 = sqrt(pow(a2, 2) + pow(b2, 2));
-    
+
     //average the two chromas
     cBar = (c1 + c2)/2;
-    
+
     g = 0.5 * (1-sqrt( pow(cBar,7) / (pow(cBar,7) + pow(25,7)) ));
-    
+
     a1Prime = a1*(1+g);
     a2Prime = a2*(1+g);
-    
+
     c1Prime = sqrt( pow(a1Prime,2) + pow(b1,2) );
     c2Prime = sqrt( pow(a2Prime,2) + pow(b2,2) );
     cBarPrime = (c1Prime+c2Prime) /2;
-    
+
     h1PrimeTerm = toDegrees(atan2(b1, a1Prime));
     h1Prime = (h1PrimeTerm >= 0) ? h1PrimeTerm : h1PrimeTerm + 360;
-    
+
     h2PrimeTerm = toDegrees(atan2(b2, a2Prime));
     h2Prime = (h2PrimeTerm >= 0) ? h2PrimeTerm : h2PrimeTerm + 360;
-    
+
     hBarPrime = (abs(h1Prime - h2Prime) > 180) ? (h1Prime + h2Prime + 360)/2 : (h1Prime + h2Prime)/2;
-    
+
     t = 1 - (0.17*cos(toRadians(hBarPrime - 30))) + (0.24*cos(toRadians(2*hBarPrime))) + (0.32*cos(toRadians(3*hBarPrime + 6))) - (0.20*cos(toRadians(4*hBarPrime - 63)));
-    
+
     //delta for hue
     deltaHPrimeTerm = h2Prime - h1Prime;
     if(abs(deltaHPrimeTerm) <= 180) { deltaHPrime= deltaHPrimeTerm; }
     else if(abs(deltaHPrimeTerm) > 180 && h2Prime <= h1Prime) { deltaHPrime= deltaHPrimeTerm - 360; }
     else { deltaHPrime= deltaHPrimeTerm + 360; }
-    
+
     //delta for lightness
     deltaLPrime= l2 - l1;
     //delta for chroma
     deltaCPrime= c2Prime - c1Prime;
-    
+
     deltaBigHPrime= 2 * sqrt(c1Prime*c2Prime) * sin(toRadians(deltaHPrime/2));
-    
+
     sL= 1 + ((0.015 * pow(lBarPrime-50,2)) / (sqrt(20 + pow(lBarPrime-50,2))));
     sC= 1 + (0.045*cBarPrime);
     sH= 1 + (0.015*cBarPrime*t);
-    
+
     deltaTheta= 30*exp(-1*pow((hBarPrime-275)/25,2));
-    
+
     rC= 2 * sqrt( pow(cBarPrime,7) / (pow(cBarPrime,7) + pow(25,7)) );
     rT= -1*rC*sin(toRadians(2*deltaTheta));
-    
+
     kL= 1;
     kC= 1;
     kH= 1;
-    
-    
+
+
     return sqrt( pow(deltaLPrime/(kL*sL),2) + pow(deltaCPrime/(kC*sC),2) + pow(deltaBigHPrime/(kH*sH),2) + (rT*(deltaCPrime/(kC*sC))*(deltaBigHPrime/(kH*sH))));
 }
 
@@ -241,15 +241,15 @@ export function xyz_to_lab(xyz) {
     x= xyz[0];
     y= xyz[1];
     z= xyz[2];
-    
+
     xn= 95.047;
     yn= 100.000
     zn= 108.883;
-    
+
     l = 116*f(y/yn) - 16;
     a = 500*(f(x/xn) - f(y/yn))
     b = 200*(f(y/yn) - f(z/zn))
-    
+
     return [l,a,b];
 }
 
@@ -264,22 +264,22 @@ export function srgb_to_xyz(rgb) {
     r= rgb[0] / 255;
     g= rgb[1] / 255;
     b= rgb[2] / 255;
-    
+
     r= linear_rgb(r);
     g= linear_rgb(g);
     b= linear_rgb(b);
-    
+
     x = (0.4124*r + 0.3576*g + 0.1805*b);
     y = (0.2126*r + 0.7152*g + 0.0722*b);
     z = (0.0193*r + 0.1192*g + 0.9505*b);
-    
+
     return [x*100,y*100,z*100];
 }
 
 function linear_rgb(c) {
     var a= 0.055;
-    
+
     c = (c > 0.04045) ? Math.pow((c+a)/(a+1), 2.4) : c/12.92;
-    
+
     return c;
 }
